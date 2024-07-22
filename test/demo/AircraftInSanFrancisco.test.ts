ba94e6a2e726695d64194a69d2558b44299eda95
Martin Schlodinski <martin.schlodinski@moia.io>
2024-07-23 00:06:03 +0200

    add tests

---
+++ test/demo/AircraftInSanFrancisco.test.ts
@@ -0,0 +1,18 @@
+import { Cartesian3 } from "cesium";
+import { getEntity } from "../../src/demo/AircraftInSanFrancisco";
+
+test("Entity position", () => {
+  const entity = getEntity();
+  expect(entity.position).toBeDefined();
+  expect(entity.position).not.toEqual(Cartesian3.fromDegrees(0, 0, 0));
+});
+
+test("Entity orientation", () => {
+  const entity = getEntity();
+  expect(entity.orientation).toBeDefined();
+});
+
+test("Entity model", () => {
+  const entity = getEntity();
+  expect(entity.model).toBeDefined();
+});
